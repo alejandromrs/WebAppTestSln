ce387baa2e9e16f5e3a1ae8ef62a29f6130700c0
Alejandro Martinez <alejandro.martinez1@se.com>
2020-08-28 14:34:35 -0500

    Adding Starter project

---
+++ WebAppTest/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page. Change over here";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Hello world";
+
+            return View();
+        }
+
+        public ActionResult Vendor()
+        {
+            ViewBag.Message = "Hello vendor";
+
+            return View();
+        }
+    }
+}
